8adc73823b8be9d527a06ee6ce4ba5d7d51d51db
dieromeo <diegoromeo99@icloud.com>
2023-12-14 20:19:16 +0100

    add: font awesome

--- src/main.js
+++ src/main.js
@@ -1,4 +1,12 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+//font awesome import
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { faStar } from '@fortawesome/free-solid-svg-icons'
+import { faStar } from '@fortawesome/free-regular-svg-icons'
 
-createApp(App).mount('#app')
+library.add(faStar)
+
+createApp(App).component('font-awesome-icon', FontAwesomeIcon)
+    .mount('#app')
