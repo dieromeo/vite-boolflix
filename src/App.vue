2ea808667d88f760a9c12b421a55e033dff97f52
dieromeo <diegoromeo99@icloud.com>
2023-12-14 15:13:52 +0100

    feat: flag

--- src/App.vue
+++ src/App.vue
@@ -8,7 +8,9 @@ export default {
     },
     data() {
         return {
-            store
+            store,
+            itaFlag: { url: '../public/ita-flag.jpg', alt: 'Italian flag' },
+            enFlag: { url: '../public/eng-flag.jpg', alt: 'English flag' }
         }
     }
 }
@@ -21,7 +23,10 @@ export default {
             <h2>Title: {{ film.title }}</h2>
             <h4>Original title: {{ film.original_title }}</h4>
             <div class="language">
-                <p>Language: {{ film.original_language }}</p>
+                <p>Language: </p>
+                <img :src="itaFlag.url" :alt="itaFlag.alt" v-if="film.original_language === 'it'">
+                <img :src="enFlag.url" :alt="itaFlag.alt" v-else-if="film.original_language === 'en'">
+                <span v-else>{{ film.original_language }}</span>
             </div>
             <p>Vote: {{ film.vote_average }}</p>
 
@@ -30,5 +35,16 @@ export default {
 </template>
 
 <style lang="scss">
-@use './style/main.scss'
+@use './style/main.scss' as *;
+
+.language {
+    display: flex;
+    align-items: center;
+
+    img {
+        display: block;
+        height: 15px;
+        margin-left: 10px;
+    }
+}
 </style>
